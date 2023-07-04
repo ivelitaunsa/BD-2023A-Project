19fe4f24d0639d371e75d6657577ff4e0a29df25
fvilcaqui <fvilcaqui@unsa.edu.pe>
2023-07-04 12:44:50 -0500

    Terminado la interfaz de Departamento

--- src/Driver.java
+++ src/Driver.java
@@ -3,9 +3,9 @@ public class Driver {
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new GZZ_CARGO().setVisible(true);
+                //new GZZ_CARGO().setVisible(true);
                 // new GZZ_ESTADO_REGISTRO().setVisible(true);
-                // new GZZ_DEPARTAMENTO().setVisible(true);
+                new GZZ_DEPARTAMENTO().setVisible(true);
                 // new GZZ_GENERO().setVisible(true);
                 // new GZZ_RANGO().setVisible(true);
                 // new GZZ_EST_ASISTENCIA().setVisible(true);
